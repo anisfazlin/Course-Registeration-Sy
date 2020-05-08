a99c2e66f32099f64948fad16d2a721653e758d7
anisfazlin <anisfazlin.adib@gmail.com>
2020-05-08 22:44:06 +0000

    Fixed submission and fallback if duplicate

--- main.c
+++ main.c
@@ -4,9 +4,11 @@
 int main()
 {
     int matricnum, x, decide=1; //x is number of courses
-    int i=0, credhour=0, totalCred=0, art=0, sci=0;
-    char code;
+    int i=0, credhour=0, totalCred=0;
+		int art=0, sci=0;
 		bool _acc=false, _bus=false, _com=false, _eng=false, _med=false, _pha=false;
+    char code;
+		
     
     do{
         
@@ -25,61 +27,101 @@ int main()
     printf("\n");
     printf("A- Accounting      B- Business      C- Computing\n");
     printf("E- Engineering     M- Medicine      P- Pharmacy\n\n");
-    
-  do{
-      printf("Enter your number of courses to be taken: ");
-      scanf("%d", &x);
-      // printf("Value of X is %d\n",x);
-	}while(x>6);
 
-	
-		if(decide==1){
+  do{
+		do{
+				printf("Enter your number of courses to be taken: ");
+				scanf("%d", &x);
+				// printf("Value of X is %d\n",x);
+		}while(x>6);
+		
 			for(i=0; i<x; i++){
 
 			printf("Enter your code course(s) #%d: ",i+1);
     	scanf(" %c", &code);
 
         if(code=='A'||code=='a'){
-					credhour+=4;
-					_acc = true;
+					if(_acc == true){
+						printf("You have registered this course. Try another course.\n");
+						i--;
+					}
+					else{
+						credhour+=4;
+						_acc = true;
+					}	
 				}
 				
         else if(code=='B'||code=='b'){
+					if(_bus == true){
+						printf("You have registered this course. Try another course.\n");
+						i--;
+					}
+					else{
 					credhour+=4;
           _bus = true;
+					}
 				}
 
 				else if(code=='C'||code=='c'){
+					if(_com == true){
+						printf("You have registered this course. Try another course.\n");
+						i--;
+					}
+					else{
 					credhour+=5;
 					_com = true;
+					}
 				}
 
 				else if(code=='E'||code=='e'){
+					if(_eng == true){
+						printf("You have registered this course. Try another course.\n");
+						i--;
+					}
+					else{
 					credhour+=5;
 					_eng = true;
+					}
 				}
 
 				else if(code=='M'||code=='m'){
+					if(_med == true){
+						printf("You have registered this course. Try another course.\n");
+						i--;
+					}
+					else{
 					credhour+=5;
 					_med = true;
+					}
 				}
 
 				else if(code=='P'||code=='p'){
+					if(_pha == true){
+						printf("You have registered this course. Try another course.\n");
+						i--;
+					}
+					else{
 					credhour+=5;
 					_pha = true;
+					}
 				}
 
 				else{
 					printf("Invalid code!\n");
           i--;
 				}
+			}
         // printf("\nCummulative cred hours: %d\n\n",credhour);
-			
-    }
+
+		
+    
+
     printf("\nWould you like to reset OR submit this course registration? \n(1-Reset 2-Submit)\n");
 			scanf("%d", &decide);
-		// else{ printf("Bye!");}
-    
+
+		 }while(decide==1);
+
+		
 		totalCred += credhour;
 
 
@@ -89,15 +131,15 @@ int main()
 // Art -> A - Accounting, B - Business            
 // Science ->C - Computing, E - Engineering, M - Medicine, P - Pharmacy
 
-    if(_acc==true) { art++;}
-    if(_bus==true) { art++;}
-    if(_com==true) { sci++;}
-    if(_eng==true) { sci++;}
-    if(_med==true) { sci++;}
-    if(_pha==true) { sci++;}
+    if(_acc==true) {art++;}
+    if(_bus==true) {art++;}
+    if(_com==true) {sci++;}
+    if(_eng==true) {sci++;}
+    if(_med==true) {sci++;}
+    if(_pha==true) {sci++;}
 
     printf("\n\n%d, you have registered %d Art course(s) and %d Science course(s). \nTotal credit hours is %d.", matricnum, art, sci, totalCred);
 
     return 0;
-  }
+  
 }
